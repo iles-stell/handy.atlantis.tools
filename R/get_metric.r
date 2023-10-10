4d34aa227658b488b9f41ae4c29f388042533553
iles_stell <ilaria.stollberg@gmail.com>
2023-10-11 07:38:05 +1100

    Update get_metric
    - Get_metric now yields last set of RelBiomass values for each group in each output folder, as well as input values for C and mQ of ZG group (work in progress to expand selection to all groups and other parameters, e.g., mum and pPrey)

--- R/get_metric.r
+++ R/get_metric.r
@@ -1,117 +1,12 @@
-# # #' Automated Atlantis calibration
-# # #' Automatise calibration of Atlantis parameters most often modified during calibration phase.
-# # #' 
-# # #' @param base_path The path to the folder containing the output folders.
-
-
-# get_metric <- function(base_path) {
-#   library(ggplot2)
-#   library(readr)
-#   library(dplyr)
-#   library(tidyr)
-
-#   getData <- function(folder) {
-
-#     # Load the XML file
-#     xml_files <- list.files(folder, pattern = "EA_biol.*\\.xml$", full.names = TRUE)
-
-#     if (length(xml_files) > 0) {
-#     xml_file <- read_xml(xml_files[1])
-
-#     # Extract value from XML file for C and mQ
-#     node_C <- xml_find_all(xml_file, "//Attribute[@AttributeName='InvertebrateClearanceRate']/GroupValue[@GroupName='ZG']")
-#     attribute_value_C <- xml_attr(node_C, "AttributeValue")
-
-#     node_mQ <- xml_find_all(xml_file, "//Attribute[@AttributeName='FLAG_MQ_T15']/GroupValue[@GroupName='ZG']")
-#     attribute_value_mQ <- xml_attr(node_mQ, "AttributeValue")
-#     } else {
-#     warning(paste("No XML file containing 'EA_biol' found in folder:", folder))
-#     }
-
-#     ### Get all subfolders that contain the word "output"
-#     subfolders <- list.dirs(folder, recursive = TRUE, full.names = TRUE)
-#     subfolders <- subfolders[grepl("output", subfolders)]
-
-#     # RelBiomass: get the file containing "BiomIndex" in each subfolder
-#     files <- lapply(subfolders, function(subfolder) {
-#       list.files(subfolder, pattern = "_BiomIndx", full.names = TRUE)[1]
-#     })
-#     files <- unlist(files)
-
-#     # Filter out NA values
-#     files <- files[!is.na(files)]
-
-#     # Read the data from each file and combine into a single data frame
-#     my_data <- lapply(files, function(file) {
-#       if(file.exists(file)) {
-#         read_table(file, col_names = TRUE)
-#       } else {
-#         warning(paste("File does not exist:", file))
-#         return(data.frame()) # returns an empty dataframe if file does not exist
-#       }
-#     })
-
-#     my_data <- bind_rows(my_data)
-#     if(nrow(my_data) == 0) return(NULL) # return NULL if no data to avoid errors in summarise_all
-
-#     my_data2 <- my_data %>%
-#       group_by(Time) %>%
-#       summarise_all(mean, na.rm = TRUE) # Added na.rm = TRUE to handle NA values in columns
-
-#     # Convert the wide format to a long format
-#     my_data2_long <- my_data2 %>%
-#       gather(key = "variable", value = "value", -Time)
-
-#     my_data2_long$Folder <- basename(folder)
-#   }
-
-# ########################
-# #  Extract RelBiomass  #
-# ########################
-
-#   out_folders <- list.dirs(path = base_path, full.names = TRUE, recursive = FALSE)
-#   out_folders <- out_folders[grepl("output", basename(out_folders))]
-
-#   # Get data from all folders
-#   all_data <- lapply(out_folders, getData)
-
-#   # Clean the all_data list to remove NULLs
-#   all_data <- all_data[!sapply(all_data, is.null)]
-
-#   if(length(all_data) == 0) {
-#     stop("No valid data found.")
-#   }
-
-#   # Combine all data into a single data frame
-#   combined_data <- bind_rows(all_data)
-
-#   # Filter the combined data to keep only variables that start with "Rel"
-#   combined_data <- combined_data[grep("^Rel", combined_data$variable), ]
-
-#   if(nrow(combined_data) == 0) {
-#     stop("No variables starting with 'Rel' found.")
-#   }
-
-#   # Adding this line to assign the last value of the dataset to a variable
-
-#   last_value <- combined_data %>%
-#   group_by(variable, Folder) %>%
-#   filter(Time == max(Time)) %>%
-#   slice(n()) %>%
-#   ungroup()
-
-#   # Print or return the last value if needed
-#   print(last_value)
-#   return(last_value)  # Or, if you need to return the last value from the function
-
-
-#     ############################
-#     #  Extract xml parameters  #
-#     ############################
-
-#   return(xml_data)
-
-# }
+#' get_metric function
+#' Extract data for RelBiomass and input parameters from Atlantis output .txt and xml files.
+#'
+#' @param base_path The path to the folder containing the output folders.
+#'
+#' @return A list of datasets for relative biomass (final/initial biomass values) and parameter values from biology input xml file.
+#' @export
+#' @examples
+#' get_metric("your/output/files/path")
 
 get_metric <- function(base_path) {
   library(xml2)
